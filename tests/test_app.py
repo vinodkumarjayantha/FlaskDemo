a4e18977e959902932ef95cb81abfddb9ddbadf7
vinodkumarjayantha <reach.jvinodkumar@gmail.com>
2026-06-16 13:50:16 +0530

    jenkins file changes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,20 @@
+import logging
 from app import app
 
+logger = logging.getLogger(__name__)
+
 def test_health():
+
+    logger.info("Starting health endpoint test")
 
     client = app.test_client()
 
     response = client.get("/health")
 
+    logger.info(f"Response status code: {response.status_code}")
+    logger.info(f"Response JSON: {response.json}")
+
     assert response.status_code == 200
     assert response.json["status"] == "UP"
+
+    logger.info("Health endpoint test completed")
